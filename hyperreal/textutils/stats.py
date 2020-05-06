134fc2848429713cebaa5ddbb65ba81bc26cde5e
wsrtka <witold.serwatka@pm.me>
2020-05-06 16:50:44 +0200

    Added code from paper notebook

--- hyperreal/textutils/stats.py
+++ hyperreal/textutils/stats.py
@@ -1,3 +1,5 @@
+from math import log
+
 
 def get_word_freq(filename, split_char):
     """
@@ -16,3 +18,17 @@ def get_word_freq(filename, split_char):
 
     return word_freq
 
+
+def compute_tf_idf(term, terms_freq, docs_terms, docs_count):
+    """
+    Compute the product of term frequency and inverse data frequency.
+    :param docs_count: int with the number of docs
+    :param term: string for which we compute the tf-idf
+    :param terms_freq: dictionary (str: int) with term frequencies
+    :param docs_terms: dictionary (str: int) with number of documents containing term
+    :return: float with the tf-idf
+    """
+    tf = terms_freq[term]
+    #TODO: wzór nie jest prawidłowy?
+    idf = log((1 + docs_count) / (1 + docs_terms[term])) + 1
+    return tf * idf
--- hyperreal/textutils/utils.py
+++ hyperreal/textutils/utils.py
@@ -1,3 +1,5 @@
+from _collections import namedtuple
+
 
 def lemmatize(token, lemma_dict):
     """
@@ -37,3 +39,93 @@ def get_lemma_dict(filename, split_char):
 
     return lemma_dict
 
+
+WordInContext = namedtuple("WordInContext", ["left_context", "word", "right_context"])
+
+
+def word_in_context(word_index, text, width):
+    """
+    Create a tuple with a word's context.
+    :param word_index: int index of word in text
+    :param text: list of strings in which to search for context
+    :param width: int radius of context
+    :return: WordInContext tuple with left, right contexts and the word
+    """
+    left_context = [text[j] for j in range(word_index - width, word_index) if j > 0]
+    right_context = [text[j] for j in range(word_index + 1, word_index + width + 1) if j < len(text)]
+    return WordInContext(left_context, text[word_index], right_context)
+
+
+def get_keywords_contexts(text, width, keywords, lemma_dict):
+    """
+    Get contexts of key words.
+    :param lemma_dict: current lemma dictionary of type (word: lemma)
+    :param text: string containing the text to analise
+    :param width: int radius of context
+    :param keywords: list of strings containing keywords for which we need contexts
+    :return: list of WordInContext tuple
+    """
+    words_in_context = []
+    tokens = text.split(" ")
+    tokens = [lemmatize(token, lemma_dict) for token in tokens]
+
+    word_index = 0
+
+    while word_index < len(tokens):
+        if tokens[word_index] in keywords:
+            words_in_context.append(word_in_context(word_index, tokens, width))
+        word_index += 1
+
+    return words_in_context
+
+
+def compare_context(a, b):
+    """
+    Compare contexts of two WordInContext tuples
+    :param a: WordInContext tuple
+    :param b: WordInContext tuple
+    :return: True if contexts match
+    """
+    return set(a.left_context) == set(b.left_context) and set(a.right_context) == set(b.right_context)
+
+
+def words_matching_context(text, contexts, width, lemma_dict):
+    """
+    Find words which match given contexts.
+    :param text: string containing text to analise
+    :param contexts: list of WordInContext tuples to compare contexts to, must be same width as width param
+    :param width: int width of contexts
+    :param lemma_dict: current lemma dictionary of type (word: lemma)
+    :return: list of strings containing words which match any of the given contexts
+    """
+    words = []
+    tokens = text.split(" ")
+    tokens = [lemmatize(token, lemma_dict) for token in tokens]
+    for i in range(len(tokens)):
+        wic = word_in_context(i, tokens, width)
+
+        for ctx in contexts:
+            if compare_context(wic, ctx):
+                words.append(wic.word)
+
+    return words
+
+
+def get_new_names(texts, contexts, keywords, width, lemma_dict):
+    """
+    Get a list of new potential names for keywords (i.e. drug names).
+    :param texts: list of string lists containing texts to analise
+    :param contexts: list of WordInContext tuples to compare contexts to, must be same width as width param
+    :param keywords: list of strings containing words we are looking for alternatives for
+    :param width: int width of contexts
+    :param lemma_dict: current lemma dictionary of type (word: lemma)
+    :return:
+    """
+    new_keywords = []
+
+    for text in texts:
+        for word in words_matching_context(text, contexts, width, lemma_dict):
+            if word not in keywords:
+                new_keywords.append(word)
+
+    return new_keywords
