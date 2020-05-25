2f0b0fb1064e690e90445e48d87cf5ccca9e343a
wsrtka <witold.serwatka@pm.me>
2020-05-25 19:38:35 +0200

    added plotting to example.py

--- exmaple.py
+++ exmaple.py
@@ -1,14 +1,33 @@
-data_csv_path = ''
+data_csv_path = 'data.csv'
+
+
 import pandas as pd
 import hyperreal.datautils.preprocess as preprocess
 import hyperreal.overview.stats as stats
+import hyperreal.overview.plotting as plot
+
+
 df = pd.read_csv(data_csv_path)
 df = preprocess.data_pre(df)
 
-print(stats.get_posts_per_year(df))
+res1 = stats.get_posts_per_year(df)
 
-print(stats.get_most_active_authors(df))
+res2 = stats.get_most_active_authors(df)
 
-print(stats.get_total_posts(df))
+res3 = (stats.get_total_posts(df))
 
-print(stats.get_forum_popularity(df, 384))
+res4, name = (stats.get_forum_popularity(df, 384))
+
+# plotting test
+
+plot.plot_post_per_year(df)
+
+plot.plot_author_activity(df)
+
+# plot.plot_total_post_count_per_forum(res3)
+
+
+print(plot.plot_forum_popularity(res4, name))
+
+
+
--- hyperreal/overview/plotting.py
+++ hyperreal/overview/plotting.py
@@ -10,7 +10,7 @@ def plot_post_per_year(data, date_column="date"):
     :return:
     """
     by_year = data[date_column].groupby([data.date.dt.year]).agg('count')
-    by_year.plot('bar')
+    by_year.plot(kind='bar')
 
 
 def plot_author_activity(data, author_column="author", post_id_column="post_id", ascending=False, top=10):
@@ -32,7 +32,7 @@ def plot_total_post_count_per_forum(to_plot):
     :param to_plot: object prepared by the get_total_posts() function in hyperreal.overview.stats
     :return:
     """
-    g = sns.catplot(x="forum_name", y="count", data=to_plot, aspect=5, hue='count', legend=False)
+    g = sns.catplot(x="name", y="count", data=to_plot, aspect=5, hue='count', legend=False)
     g.set_xticklabels(rotation=90)
 
 
@@ -44,7 +44,7 @@ def plot_forum_popularity(timeframe, forum_name):
     :return:
     """
     sns.set_style("whitegrid")
-    g = sns.relplot(x='date', y='post_count', hue='post_count', aspect=2, data=timeframe)
+    g = sns.relplot(x='date', y='post_count', hue='post_count', data=timeframe, aspect=2)
     g.fig.autofmt_xdate()
     g.ax.set_title(forum_name)
 
